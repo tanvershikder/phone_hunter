e6d595a5a971a13d69a235ec95610b1eaaa7e917
Your Name <you@example.com>
2022-02-28 22:23:48 +0600

    second commit

--- js/index.js
+++ js/index.js
@@ -1,14 +1,23 @@
 // api call
 const searchPhone = () =>{
+    const errorMsg = document.getElementById('error');
     const searchFild = document.getElementById('search_fild')
     const searchText = searchFild.value ;
     console.log(searchText);
     //clear the value
     searchFild.value = '';
 
-    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
-    .then(res => res.json())
-    .then(data => displaySearch(data.data));
+    // validation chack 
+    if(searchText == false){
+        errorMsg.style.display = 'inline-block';
+        // alert("write somethig to display");
+    }
+
+    else{
+        fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+        .then(res => res.json())
+        .then(data => displaySearch(data.data));
+    }
 }
 //display search
  const displaySearch = phones =>{
@@ -22,11 +31,11 @@ const searchPhone = () =>{
          const div = document.createElement('div');
          div.classList.add('col')
          div.innerHTML = `
-         <div class="card h-100 rounded">
+         <div class="card h-50 rounded">
             <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">${phone.phone_name}</h5>
-                <p class="card-text">${phone.brand}</p>
+                <h5 class="card-title primary">Model : ${phone.phone_name}</h5>
+                <p class="card-text primary2">Brand : ${phone.brand}</p>
                 <button onClick="loadMealDetails('${phone}')"  class="buttonStyle">Details</button>
             </div>
             
